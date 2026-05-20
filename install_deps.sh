d4b24cfc31f1eea1979b9e90b81c818ade34a3d7
luiskallak-design <luiskallak@gmail.com>
2026-05-20 20:54:39 -0300

    Update install_deps.sh

--- install_deps.sh
+++ install_deps.sh
@@ -7,25 +7,25 @@ GREEN='\033[0;32m'
 RED='\033[0;31m'
 RESET='\033[0m'
 
-echo -e "${CYAN)⌬ [ARCHON-SYSTEM] INICIANDO RECALIBRAGEM DE DEPENDÊNCIAS...${RESET}"
+echo -e "${CYAN}⌬ [ARCHON-SYSTEM] INICIANDO RECALIBRAGEM DE DEPENDÊNCIAS...${RESET}"
 
 # Detecção da Distro
 if [ -f /etc/arch-release ]; then
-    echo -e "${GOLD)❱❱ SISTEMA DETECTADO: ARCH LINUX${RESET}"
+    echo -e "${GOLD}❱❱ SISTEMA DETECTADO: ARCH LINUX${RESET}"
     sudo pacman -S --needed --noconfirm sdl2 sdl2_mixer ncurses qterminal nsxiv chromium
 
 elif [ -f /etc/debian_version ]; then
-    echo -e "${GOLD)❱❱ SISTEMA DETECTADO: DEBIAN/UBUNTU/MINT${RESET}"
+    echo -e "${GOLD}❱❱ SISTEMA DETECTADO: DEBIAN/UBUNTU/MINT${RESET}"
     sudo apt update
     sudo apt install -y libsdl2-dev libsdl2-mixer-dev libncursesw5-dev qterminal nsxiv chromium-browser
 
 elif [ -f /etc/fedora-release ]; then
-    echo -e "${GOLD)❱❱ SISTEMA DETECTADO: FEDORA${RESET}"
+    echo -e "${GOLD}❱❱ SISTEMA DETECTADO: FEDORA${RESET}"
     sudo dnf install -y SDL2-devel SDL2_mixer-devel ncurses-devel qterminal nsxiv chromium
 
 else
-    echo -e "${RED)⌬ ERRO: DISTRO NÃO RECONHECIDA. POR FAVOR, INSTALE AS DEPENDÊNCIAS MANUALMENTE.${RESET}"
+    echo -e "${RED}⌬ ERRO: DISTRO NÃO RECONHECIDA. POR FAVOR, INSTALE AS DEPENDÊNCIAS MANUALMENTE.${RESET}"
     exit 1
 fi
 
-echo -e "\n${GREEN)⌬ AMBIENTE PRONTO! AGORA É SÓ DAR 'MAKE' E SUBIR AO OLIMPO.${RESET}"
+echo -e "\n${GREEN}⌬ AMBIENTE PRONTO! AGORA É SÓ DAR 'MAKE' E SUBIR AO OLIMPO.${RESET}"
